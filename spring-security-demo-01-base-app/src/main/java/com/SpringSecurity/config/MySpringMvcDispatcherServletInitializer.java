75fc1ca9a6646bdd3328557c9083cea5c1b4bffa
chedjou03 <chedjou03@yahoo.fr>
2018-11-04 00:02:30 -0400

    second commit

--- spring-security-demo-01-base-app/src/main/java/com/SpringSecurity/config/MySpringMvcDispatcherServletInitializer.java
+++ spring-security-demo-01-base-app/src/main/java/com/SpringSecurity/config/MySpringMvcDispatcherServletInitializer.java
@@ -12,14 +12,14 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new Class[] {DemoAppConfig.class};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new String [] {"/"};
 	}
 
 }
